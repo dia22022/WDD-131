d8fdbb6e25fb72d32c5badd41e22081e69e96e8a
dia22022 <145490860+dia22022@users.noreply.github.com>
2024-10-16 18:50:53 -0600

    commit

---
+++ coolpics/coolpics.js
@@ -0,0 +1,38 @@
+const button = document.querySelector("button")
+button.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+    const menu = document.querySelector(".nav")
+    menu.classList.toggle("show")
+}
+
+function handleResize() {
+    const menu = document.querySelector("button")
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide")
+    } else {
+        menu.classList.add("hide")
+    }
+}
+handleResize()
+window.addEventListener("resize", handleResize)
+
+const imgs = document.querySelectorAll('img')
+imgs.forEach((ele) => {
+    ele.addEventListener("click", viewHandler)
+})
+
+function viewHandler(event) {
+	// create a variable to hold the element that was clicked on from event.target
+    //console.log('hello')
+    
+	// get the src attribute from that element and 'split' it on the "-"
+
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+
+	// insert the viewerTemplate into the top of the body element
+	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+
+}
