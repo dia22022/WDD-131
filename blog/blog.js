fe9c82dcbaf2b044c19ce5d3a6516c678946cd8a
dia22022 <145490860+dia22022@users.noreply.github.com>
2024-10-24 08:06:39 -0600

    commit

--- blog/blog.js
+++ blog/blog.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('#article-container');
+
 const articles = [
 	{
 		id: 1,
@@ -23,5 +25,62 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 2,
+		title: "Magnus Chase Book One: Sword of Summer",
+		date: "December 12, 2021",
+		description:
+		"The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.",
+		imgSrc:
+		"https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300",
+		imgAlt: "Book cover for Magnus Chase 1",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐"
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		'A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his "Aunt Pol" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.',
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
-]
+];
+
+articles.forEach(article => {
+    // Create an article element
+    const articleElement = document.createElement('article');
+    articleElement.classList.add('article');
+})
+function bookTemplate (book){
+	return `
+	<div class="article-container">
+		<div class="left-info">
+			<p class="date">${book.date}</p> 
+			<p class="age">${book.age}</p>
+			<p class="genre">${book.genre}</p>
+			<p class="stars">${book.stars}</p>
+		</div>
+		<div class="right-info">
+			<h2 class="title">${book.title}</h2>
+			<img class="img" src="${book.imgSrc}" alt="${book.imgAlt}"></img>
+			<p class="description">${book.description}</p>
+		</div>
+	</div>`
+}
+const artiesElement = document.getElementById("articles")
+
+
+for (let i=0; i<articles.length; i++){
+	const article = articles[i]
+	const htmltoinsert = bookTemplate(article)
+
+	artiesElement.insertAdjacentHTML("afterbegin", htmltoinsert)
+}
